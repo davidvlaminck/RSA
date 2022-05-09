8b1644c4b5250ab630126cf08e99d04d536bb090
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-05-09 12:14:16 +0200

    added frequence as param

--- Report.py
+++ Report.py
@@ -1,5 +1,5 @@
 class Report:
-    def __init__(self, name: str = '', title: str = '', spreadsheet_id: str = '', datasource: str = '', add_filter: bool = True):
+    def __init__(self, name: str = '', title: str = '', spreadsheet_id: str = '', datasource: str = '', add_filter: bool = True, frequency: int = 1):
         self.summary_sheet_id = '1s_oro-4tJy_1R1G99TTPnL4-5ACR4BD-R1XWFvFuviQ'
         self.name = name
         self.title = title
@@ -7,3 +7,4 @@ class Report:
         self.datasource = datasource
         self.result_query = ''
         self.add_filter = add_filter
+        self.frequency = frequency
