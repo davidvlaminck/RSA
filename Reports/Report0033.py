ee3577821bc3676960aa495693d57894d1a484a6
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-07-20 08:45:25 +0200

    VLAN objecten hebben een HoortBij relatie met een L2AccesStructuur

--- Reports/Report0033.py
+++ Reports/Report0033.py
@@ -1,22 +1,16 @@
 from DQReport import DQReport
-from Neo4JConnector import SingleNeo4JConnector
-from SheetsWrapper import SingleSheetsWrapper
 
-if __name__ == '__main__':
-    SingleSheetsWrapper.init(service_cred_path='C:\\resources\\driven-wonder-149715-ca8bdf010930.json',
-                             readonly_scope=False)
-    SingleNeo4JConnector.init("bolt://localhost:7687", "neo4jPython", "python")
 
-    r = DQReport(name='report0032',
-                 title="Netwerkpoorten met type 'UNI' hebben een hoortbij relatie naar installatie VLAN",
-                 spreadsheet_id='14ecrURzs2O61GsGpiFvd85MuwX-A69gbh6tP37X3idQ',
-                 datasource='Neo4J',
-                 persistent_column='C')
+r = DQReport(name='report0033',
+             title="VLAN objecten hebben een HoortBij relatie met een L2AccesStructuur",
+             spreadsheet_id='12urCVlUXm_KbNCrQS1MH5kVqNKak3n5DdQlAnk8mn9w',
+             datasource='Neo4J',
+             persistent_column='C')
 
-    # query that fetches uuids of results
-    result_query = """MATCH (n:Netwerkpoort {isActief:TRUE, type:'UNI'}) 
-WHERE NOT EXISTS ((n)-[:HoortBij]->(:VLAN {isActief:TRUE})) 
+# query that fetches uuids of results
+result_query = """MATCH (n:VLAN {isActief:TRUE}) 
+WHERE NOT EXISTS ((n)-[:HoortBij]->(:L2AccessStructuur {isActief:TRUE}))
 RETURN n.uuid, n.naam"""
 
-    r.result_query = result_query
-    r.run_report()
+r.result_query = result_query
+r.run_report()
