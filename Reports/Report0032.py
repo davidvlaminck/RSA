7b5d852ed0c95052ed542e9d53a6157f2ada4d12
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-07-20 08:18:16 +0200

    Netwerkpooorten hebben een type

---
+++ Reports/Report0032.py
@@ -0,0 +1,17 @@
+from DQReport import DQReport
+
+
+if __name__ == '__main__':
+    r = DQReport(name='report0032',
+                 title="Netwerkpooorten hebben een type",
+                 spreadsheet_id='1CNSGgZbARVwRzrMB5a2LrSz-HJblvzAGWODsPOFE1jo',
+                 datasource='Neo4J',
+                 persistent_column='C')
+
+    # query that fetches uuids of results
+    result_query = """MATCH (n:Netwerkpoort {isActief:TRUE}) 
+WHERE n.type IS NULL 
+RETURN n.uuid, n.naam"""
+
+    r.result_query = result_query
+    r.run_report()
