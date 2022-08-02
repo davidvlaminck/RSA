6645b85f27960a564ef42044fb4fe22904e6230e
davidvlaminck <davidvlaminck85@gmail.com>
2022-08-02 11:23:36 +0200

    formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ReportLoopRunner import ReportLoopRunner
 
 if __name__ == '__main__':
-    reportlooprunner = ReportLoopRunner(settings_path=r'C:\resources\settings_RSA.json')
+    reportlooprunner = ReportLoopRunner(settings_path=r'/home/davidlinux/Documents/AWV/resources/settings_RSA.json')
+    # reportlooprunner = ReportLoopRunner(settings_path=r'C:\resources\settings_RSA.json')
     reportlooprunner.run()
