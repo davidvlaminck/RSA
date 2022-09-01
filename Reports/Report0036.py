169f48233fe6960ecf221567acd3636fd095ed2b
davidvlaminck <davidvlaminck85@gmail.com>
2022-09-01 09:06:58 +0200

    added missing report

---
+++ Reports/Report0036.py
@@ -0,0 +1,20 @@
+from DQReport import DQReport
+
+
+class Report0036:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0036',
+                               title="Netwerkpoorten met type 'UNI' hebben een hoortbij relatie naar installatie VLAN",
+                               spreadsheet_id='14ecrURzs2O61GsGpiFvd85MuwX-A69gbh6tP37X3idQ',
+                               datasource='Neo4J',
+                               persistent_column='C')
+
+        self.report.result_query = """MATCH (n:Netwerkpoort {isActief:TRUE, type:'UNI'}) 
+        WHERE NOT EXISTS ((n)-[:HoortBij]->(:VLAN {isActief:TRUE}))
+        RETURN n.uuid, n.naam"""
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
