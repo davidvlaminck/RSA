c64579fcfd06236d248b83d54accf5975964aed3
greniegw <gwij.grenie@mow.vlaanderen.be>
2022-10-27 10:29:15 +0200

    Add report
    This report checks the following data criteria:
    
    "EnergiemeterDNB en ForfaitaireAansluiting worden gevoed door een DNBLaagspanning."

---
+++ Reports/Report0042.py
@@ -0,0 +1,20 @@
+from DQReport import DQReport
+
+
+class Report0042:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0042',
+                               title='EnergiemeterDNB en ForfaitaireAansluiting worden gevoed door een DNBLaagspanning',
+                               spreadsheet_id='',
+                               datasource='Neo4J',
+                               persistent_column='D')
+
+        self.report.result_query = """PROFILE MATCH (x {isActief: TRUE})
+            WHERE (x:EnergiemeterDNB OR x:ForfaitaireAansluiting) AND NOT EXISTS((x)<-[:Voedt]-(:DNBLaagspanning {isActief: TRUE}))
+            RETURN x.uuid as uuid, x.naam as naam, x.typeURI as typeURI"""
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
