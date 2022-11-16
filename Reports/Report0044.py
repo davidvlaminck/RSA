aeb6bba39ef66970da24ea5ce682426fcb3ae81d
greniegw <gwij.grenie@mow.vlaanderen.be>
2022-11-16 10:31:16 +0100

    Add Report0044.py
    This report handles the following data criteria:
    
    "Er zijn geen classes met ingevulde attributen die deprecated zijn in de OTL"

---
+++ Reports/Report0044.py
@@ -0,0 +1,33 @@
+from DQReport import DQReport
+from OTLCursorPool import OTLCursorPool
+
+
+class Report0044:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0044',
+                               title='Er zijn geen classes met ingevulde attributen die deprecated zijn in de OTL',
+                               spreadsheet_id='',
+                               datasource='PostGIS',
+                               persistent_column='E')
+
+        otl_cursor = OTLCursorPool.get_cursor()
+        deprecated_attributes = otl_cursor.execute("""
+            SELECT o_a.uri
+            FROM OSLOAttributen o_a
+            WHERE o_a.deprecated_version IS NOT NULL AND o_a.deprecated_version != ""
+        """).fetchall()
+
+        self.report.result_query = """
+            SELECT a.uuid AS asset_uuid, a.toestand AS asset_toestand, ab.uri AS attribuut_uri, a_w.waarde AS attribuut_waarde
+            FROM assets a
+            INNER JOIN attribuutwaarden AS a_w ON (a.uuid = a_w.assetuuid)
+            INNER JOIN attributen AS ab ON (a_w.attribuutuuid = ab.uuid)
+            INNER JOIN (values {}) AS d_a(uri) ON (ab.uri = d_a.uri)
+            WHERE a.actief = TRUE AND a_w NOTNULL
+        """.format(",".join(["({})".format(d) for d in deprecated_attributes]))
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
