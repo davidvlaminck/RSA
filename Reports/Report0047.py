dd6c3ca30e3c14b3fa0deb59ab639158f5197cb1
greniegw <gwij.grenie@mow.vlaanderen.be>
2022-11-23 09:10:26 +0100

    Refine Report0047.py
    Improve query for Report0047 to use URI's instead of UUID's for asset and relation types + minor changes.

--- Reports/Report0047.py
+++ Reports/Report0047.py
@@ -7,30 +7,33 @@ class Report0047:
 
     def init_report(self):
         self.report = DQReport(name='report0047',
-                               title='(Experimenteel) Wanneer een BitumineuzeLaag geometrisch op een Onderbouw ligt, dan is er een LigtOp relatie van die BitumineuzeLaag naar die Onderbouw.',
+                               title='Wanneer een BitumineuzeLaag geometrisch op een Onderbouw ligt, dan zijn deze assets verbonden door een LigtOp relatie van BitumineuzeLaag naar Onderbouw.',
                                spreadsheet_id='',
                                datasource='PostGIS',
                                persistent_column='C')
 
         self.report.result_query = """
-            WITH a_bl AS (
-                SELECT *
+            WITH a_bl as (
+                SELECT a.uuid, g.wkt_string
                 FROM assets a
+                INNER JOIN assettypes a_t ON (a.assettype = a_t.uuid)
                 INNER JOIN geometrie g ON (g.assetuuid = a.uuid)
-                WHERE a.assettype = '3d24792a-6941-481b-9c8c-739309fd3ffb')
+                WHERE a_t.uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#BitumineuzeLaag')
             , a_ob AS (
-                SELECT *
+                SELECT a.uuid, g.wkt_string 
                 FROM assets a
+                INNER JOIN assettypes a_t ON (a.assettype = a_t.uuid)
                 INNER JOIN geometrie g ON (g.assetuuid = a.uuid)
-                WHERE a.assettype = '3d24792a-6941-481b-9c8c-739309fd3ffb')
-            SELECT a_bl_prime.uuid AS uuid_bitumineuze_laag, a_ob_prime AS uuid_onderbouw
+                WHERE a_t.uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Onderbouw')
+            SELECT a_bl_prime.uuid AS uuid_bitumineuze_laag, a_ob_prime.uuid AS uuid_onderbouw
             FROM a_bl AS a_bl_prime, a_ob AS a_ob_prime
             WHERE ST_Intersects(ST_GeogFromText(a_bl_prime.wkt_string), ST_GeogFromText(a_ob_prime.wkt_string))
-            AND NOT exists (
+            AND NOT EXISTS (
                 SELECT 1
                 FROM a_bl, a_ob
                 INNER JOIN assetrelaties a_r ON (a_r.bronuuid = a_bl_prime.uuid AND a_r.doeluuid = a_ob_prime.uuid)
-                WHERE a_r.relatietype = '321c18b8-92ca-4188-a28a-f00cdfaa0e31'
+                INNER JOIN relatietypes r_t ON (a_r.relatietype = r_t.uuid)
+                WHERE r_t.uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#LigtOp'
                 LIMIT 1
             )
         """
