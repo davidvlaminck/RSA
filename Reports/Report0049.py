4f90d11cccf538267972ca5a571b97c224c8fcbc
greniegw <gwij.grenie@mow.vlaanderen.be>
2022-11-29 13:26:24 +0100

    Minor changes

--- Reports/Report0049.py
+++ Reports/Report0049.py
@@ -13,33 +13,33 @@ class Report0049:
                                persistent_column='E')
 
         self.report.result_query = """
-            MATCH (ac:IMKLActivityComplex) // activity mandatory
-            WHERE ac.`grp:activity` IS NULL
-            RETURN ac.uuid as uuid, 'activity' as property, 'is null/empty but mandatory' as fault, ac.`grp:activity` as property_value
+            MATCH (x:IMKLActivityComplex) // activity mandatory
+            WHERE x.`grp:activity` IS NULL
+            RETURN x.uuid as uuid, 'activity' as property, 'is null/empty but mandatory' as fault, x.`grp:activity` as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // activity max length
-            WHERE ac.`grp:activity` IS NOT NULL AND size(ac.`grp:activity`) > 20
-            RETURN ac.uuid as uuid, 'activity' as property, 'max length is 20' as fault, ac.`grp:activity` as property_value
+            MATCH (x:IMKLActivityComplex) // activity max length
+            WHERE x.`grp:activity` IS NOT NULL AND size(x.`grp:activity`) > 20
+            RETURN x.uuid as uuid, 'activity' as property, 'max length is 20' as fault, x.`grp:activity` as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // inNetwork mandatory
-            WHERE ac.`grp:inNetwork` IS NULL
-            RETURN ac.uuid as uuid, 'inNetwork' as property, 'is null/empty but mandatory' as fault, ac.`grp:inNetwork` as property_value
+            MATCH (x:IMKLActivityComplex) // inNetwork mandatory
+            WHERE x.`grp:inNetwork` IS NULL
+            RETURN x.uuid as uuid, 'inNetwork' as property, 'is null/empty but mandatory' as fault, x.`grp:inNetwork` as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // inNetwork enumeration
-            WHERE ac.`grp:inNetwork` IS NOT NULL AND NOT ac.`grp:inNetwork` IN ['electricity','telecommunications','crossTheme']
-            RETURN ac.uuid as uuid, 'inNetwork' as property, 'value not in enumeration' as fault, ac.`grp:inNetwork` as property_value
+            MATCH (x:IMKLActivityComplex) // inNetwork enumeration
+            WHERE x.`grp:inNetwork` IS NOT NULL AND NOT x.`grp:inNetwork` IN ['electricity','telecommunications','crossTheme']
+            RETURN x.uuid as uuid, 'inNetwork' as property, 'value not in enumeration' as fault, x.`grp:inNetwork` as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // name max length
-            WHERE ac.`grp:name` IS NOT NULL AND size(ac.`grp:name`) > 200
-            RETURN ac.uuid as uuid, 'name' as property, 'max length is 200' as fault, ac.`grp:name` as property_value
+            MATCH (x:IMKLActivityComplex) // name max length
+            WHERE x.`grp:name` IS NOT NULL AND size(x.`grp:name`) > 200
+            RETURN x.uuid as uuid, 'name' as property, 'max length is 200' as fault, x.`grp:name` as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // uuid mandatory
-            WHERE ac.uuid IS NULL
-            RETURN ac.uuid as uuid, 'uuid' as property, 'is null/empty but mandatory' as fault, ac.uuid as property_value
+            MATCH (x:IMKLActivityComplex) // uuid mandatory
+            WHERE x.uuid IS NULL
+            RETURN x.uuid as uuid, 'uuid' as property, 'is null/empty but mandatory' as fault, x.uuid as property_value
             UNION
-            MATCH (ac:IMKLActivityComplex) // uuid max length
-            WHERE ac.uuid IS NOT NULL AND size(ac.uuid) > 50
-            RETURN ac.uuid as uuid, 'activity' as property, 'max length is 50' as fault, ac.uuid as property_value
+            MATCH (x:IMKLActivityComplex) // uuid max length
+            WHERE x.uuid IS NOT NULL AND size(x.uuid) > 50
+            RETURN x.uuid as uuid, 'activity' as property, 'max length is 50' as fault, x.uuid as property_value
         """
 
     def run_report(self, sender):
