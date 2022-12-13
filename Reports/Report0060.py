6da6fb3728a53adbf9746a6c6d3c0b64c4599276
greniegw <gwij.grenie@mow.vlaanderen.be>
2022-12-13 15:23:08 +0100

    Rename Report0059.py to Report0060.py and finish report
    This report handles the following data criteria:
    
    'Er zijn geen conflicten tussen EAN-nummers (dubbel EAN-nummer).'

--- Reports/Report0060.py
+++ Reports/Report0060.py
@@ -7,23 +7,21 @@ class Report0060:
 
     def init_report(self):
         self.report = DQReport(name='report0060',
-                               title='Het EAN nummer van een DNBLaagspanning of DNBHoogspanning is geldig..',
+                               title='Er zijn geen conflicten tussen EAN-nummers (dubbel EAN-nummer).',
                                spreadsheet_id='',
                                datasource='Neo4J',
-                               persistent_column='I')
+                               persistent_column='G')
 
         self.report.result_query = """
-            WITH a, split(a.eanNummer, "") as split_ean
-            WITH a, split_ean, reduce(
-                    acc = [0, 0, 0], 
-                    n IN split_ean[0..-1] | [acc[0] + 1, acc[1] + ((acc[0] + 1) % 2) * toInteger(n), acc[2] + (acc[0] % 2) * toInteger(n)]
-                ) as running_sums
-            WITH a, ((3 * running_sums[1] + running_sums[2] + toInteger(last(split_ean))) % 10) = 0 as is_valid_ean
-            WHERE 
-                (a:DNBLaagspanning OR a:DNBHoogspanning)
-                AND (a.eanNummer IS NOT NULL) 
-                AND (NOT (a.eanNummer =~ '^54\d{16}$' OR a.eanNummer =~ '^54\d{10}$') OR NOT is_valid_ean)
-            RETURN a.uuid as uuid, a.naam as naam, a.typeURI as typeURI, a.toestand as toestand, a.eanNummer as eanNummer, a.`tz:toezichter.tz:voornaam` as tz_voornaam, a.`tz:toezichter.tz:naam` as tz_naam, a.`tz:toezichter.tz:email` as tz_email
+            MATCH (x:Asset{isActief: True})-[:HoortBij]-(y{isActief: True}) 
+            WHERE ((y:DNBHoogspanning) OR (y:DNBLaagspanning)) AND  x.eanNummer <> y.eanNummer
+            RETURN x.uuid as uuid, x.naampad as naampad, x.toestand as toestand, x.`tz:toezichter.tz:voornaam` as tz_voornaam, x.`tz:toezichter.tz:naam` as tz_naam, x.`tz:toezichter.tz:email` as tz_email
+            UNION 
+            MATCH (x:Asset{isActief: True})-[:HoortBij]-(y{isActief: True}) 
+            WHERE (y:DNBHoogspanning) OR (y:DNBLaagspanning)
+            WITH x, count(DISTINCT y.eanNummer) as ean_count 
+            WHERE ean_count > 1
+            RETURN x.uuid as uuid, x.naampad as naampad, x.toestand as toestand, x.`tz:toezichter.tz:voornaam` as tz_voornaam, x.`tz:toezichter.tz:naam` as tz_naam, x.`tz:toezichter.tz:email` as tz_email
         """
 
     def run_report(self, sender):
