21e2ef09d841a8f3512a739a72b15acdbb9570b6
gwij-mow <gwij.grenie@mow.vlaanderen.be>
2023-02-02 09:56:52 +0100

    Changes to Report0059.py
    - Add filter on toestand

--- Reports/Report0059.py
+++ Reports/Report0059.py
@@ -13,7 +13,7 @@ class Report0059:
                                persistent_column='E')
 
         self.report.result_query = """
-            MATCH p=(x:Asset {isActief: True})-[:Voedt*]->(x:Asset {isActief: True})
+            MATCH p=(x:Asset {isActief: True, toestand: "in-gebruik"})-[:Voedt*]->(x:Asset {isActief: True, toestand: "in-gebruik"})
             WITH x, reduce(path_loop = [], n IN nodes(p) | path_loop + [[n.uuid, n.typeURI]]) as path_loop
             RETURN DISTINCT x.uuid AS uuid, x.naampad AS naampad, x.typeURI AS typeURI, path_loop
         """
