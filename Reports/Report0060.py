14522fea098eaaca0989d5dcf07da1231ede5ff4
gwij-mow <gwij.grenie@mow.vlaanderen.be>
2023-02-02 09:55:49 +0100

    Changes to Report0060.py
    - Add filter on toestand

--- Reports/Report0060.py
+++ Reports/Report0060.py
@@ -13,14 +13,14 @@ class Report0060:
                                persistent_column='G')
 
         self.report.result_query = """
-            MATCH (x:Asset{isActief: True})-[:HoortBij]-(y{isActief: True}) 
+            MATCH (x:Asset{isActief: True, toestand: "in-gebruik"})-[:HoortBij]-(y{isActief: True, toestand: "in-gebruik"}) 
             WHERE ((y:DNBHoogspanning) OR (y:DNBLaagspanning)) AND  x.eanNummer <> y.eanNummer
             RETURN 
                 x.uuid as uuid, x.naampad as naampad, x.toestand as toestand, 
                 x.`tz:toezichter.tz:voornaam` as tz_voornaam, x.`tz:toezichter.tz:naam` as tz_naam, x.`tz:toezichter.tz:email` as tz_email,
                 x.`tz:toezichtgroep.tz:naam` as tzg_naam,  x.`tz:toezichtgroep.tz:referentie` as tzg_referentie
             UNION 
-            MATCH (x:Asset{isActief: True})-[:HoortBij]-(y{isActief: True}) 
+            MATCH (x:Asset{isActief: True, toestand: "in-gebruik"})-[:HoortBij]-(y{isActief: True, toestand: "in-gebruik"}) 
             WHERE (y:DNBHoogspanning) OR (y:DNBLaagspanning)
             WITH x, count(DISTINCT y.eanNummer) as ean_count 
             WHERE ean_count > 1
