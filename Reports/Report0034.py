33425be7d4d108b38eacbc954cfc9465a0e14b2f
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-04-14 15:28:16 +0200

    filter for L2 switches

--- Reports/Report0034.py
+++ Reports/Report0034.py
@@ -12,7 +12,7 @@ class Report0034:
                                datasource='Neo4J',
                                persistent_column='C')
 
-        self.report.result_query = """MATCH (n:Netwerkpoort {isActief:TRUE, type:'NNI'})
+        self.report.result_query = """MATCH (n:Netwerkpoort {isActief:TRUE, type:'NNI'})-[:Bevestiging]-(:Netwerkelement {isActief:TRUE, gebruik:'l2-switch'}) 
         WHERE NOT EXISTS ((n)-[:Sturing]-(:Netwerkpoort {isActief:TRUE, type:'NNI'}))
         RETURN n.uuid, n.naam"""
 
