c8fae6023dbeb571629bcf0517d26aa7f703fbf2
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-05-19 15:31:28 +0200

    SPARQL

--- Reports/Report0058.py
+++ Reports/Report0058.py
@@ -25,3 +25,38 @@ class Report0058:
 
     def run_report(self, sender):
         self.report.run_report(sender=sender)
+
+# sparql query:
+
+# PREFIX inst: <https://lgc.data.wegenenverkeer.be/ns/installatie#>
+# PREFIX imel: <https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#>
+# PREFIX ond: <https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#>
+# PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+# PREFIX toezicht: <https://wegenenverkeer.data.vlaanderen.be/oef/toezicht/>
+#
+# select ?doel ?naampad ?toestand ?toezichter_voornaam ?toezichter_naam ?toezichter_email ?toezichtgroep_naam ?toezichtgroep_ref where {
+#     ?doel imel:NaampadObject.naampad ?naampad ;
+#         imel:AIMToestand.toestand ?toestand .
+#     OPTIONAL {
+#         ?doel toezicht:Toezicht.toezichter ?toezichter .
+#         ?toezichter toezicht:DtcToezichter.voornaam ?toezichter_voornaam ;
+#             toezicht:DtcToezichter.naam ?toezichter_naam ;
+#             toezicht:DtcToezichter.email ?toezichter_email } .
+#     OPTIONAL {
+#         ?doel toezicht:Toezicht.toezichtgroep ?toezichtgroep .
+#         ?toezichtgroep toezicht:DtcToezichtGroep.naam ?toezichtgroep_naam ;
+#         	toezicht:DtcToezichtGroep.referentie ?toezichtgroep_ref } .
+#     {
+#         select ?doel where {
+#             ?bron imel:AIMDBStatus.isActief "true"^^xsd:boolean .
+#             ?doel imel:AIMDBStatus.isActief "true"^^xsd:boolean .
+#             ?voedt a ond:Voedt .
+#             ?voedt imel:RelatieObject.doel ?doel .
+#             ?voedt imel:RelatieObject.bron ?bron .
+#             FILTER (NOT EXISTS {?bron a inst:UPSLegacy})
+#         }
+#         GROUP BY ?doel ?naampad ?toestand
+#         HAVING(COUNT(?voedt) > 1)
+#         order by ?doel
+# 	}
+# }
