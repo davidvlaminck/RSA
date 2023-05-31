5d5d5df4215ae7423f47fa5d6852ce2335cac1b9
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-05-31 10:39:43 +0200

    restarting service every x minutes

---
+++ restart_service.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import time
+
+if __name__ == '__main__':
+    password = 'pwd'
+
+    while True:
+        command = 'sudo -S systemctl status neo4j'
+        output = subprocess.check_output('echo %s | %s' % (password, command), shell=True)
+        output_lines = output.decode('utf-8').split('\n')
+
+        line_active = [line for line in output_lines if 'Active:' in line][0]
+        line_active = line_active.split('Active:', 1)[1].strip()
+        if line_active.startswith('active'):
+            print('still active')
+            time.sleep(300)
+        else:
+            print('not active')
+            command = 'sudo -S systemctl start neo4j'
+            os.system('echo %s | %s' % (password, command))
+
+
