bf54e268473e32a884ad24c54e534b0436c5b662
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-06-01 16:40:32 +0200

    restarting service every x minutes

--- restart_service.py
+++ restart_service.py
@@ -7,17 +7,20 @@ if __name__ == '__main__':
 
     while True:
         command = 'sudo -S systemctl status neo4j'
-        output = subprocess.check_output('echo %s | %s' % (password, command), shell=True)
-        output_lines = output.decode('utf-8').split('\n')
+        try:
+            output = subprocess.check_output('echo %s | %s' % (password, command), shell=True)
+            output_lines = output.decode('utf-8').split('\n')
 
-        line_active = [line for line in output_lines if 'Active:' in line][0]
-        line_active = line_active.split('Active:', 1)[1].strip()
-        if line_active.startswith('active'):
-            print('still active')
-            time.sleep(300)
-        else:
-            print('not active')
+            line_active = [line for line in output_lines if 'Active:' in line][0]
+            line_active = line_active.split('Active:', 1)[1].strip()
+            if line_active.startswith('active'):
+                print('still active')
+                time.sleep(300)
+            else:
+                print('not active')
+                command = 'sudo -S systemctl start neo4j'
+                os.system('echo %s | %s' % (password, command))
+        except:
+            print('probably not active')
             command = 'sudo -S systemctl start neo4j'
             os.system('echo %s | %s' % (password, command))
-
-
