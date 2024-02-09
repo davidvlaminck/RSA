51ca02f3df1b429dd59f128b3cd4c1d6d3547397
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-02-09 10:11:54 +0100

    fix for neo4j clean data

--- DQReport.py
+++ DQReport.py
@@ -165,7 +165,7 @@ class DQReport(Report):
             headerrow.append('opmerkingen (blijvend)')
         result.append(headerrow)
 
-        result_data = self.clean(result_data, headerrow=headerrow)
+        result_data = self.clean(result_data, headerrow=result_keys)
 
         for data in result_data:
             row = []
