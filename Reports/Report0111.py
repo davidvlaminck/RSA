b2bf3ec860e899f0b69c5ec53fb2712e4608e362
VerdoodtDries <dries@nordend.eu>
2024-04-19 08:28:32 +0200

    Append initial reports 0111 and 0112.

---
+++ Reports/Report0111.py
@@ -0,0 +1,21 @@
+from DQReport import DQReport
+
+
+class Report0106:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0111', title='Verkeersregelaars hebben een vplanNummer volgens bepaalde syntax',
+                               spreadsheet_id='14g-EraRtSgEjC_f--Axt-6ZFWmgWvLhIXDvfw4DMBWQ', datasource='Neo4J',
+                               persistent_column='D')
+
+        self.report.result_query = """
+        // Verkeersregelaars hebben een vplannummer volgens bepaalde syntax
+        MATCH (a:Verkeersregelaar {isActief:TRUE}) 
+        WHERE NOT a.vplanNummer =~ '^[Vv]\d{5,6}[vVwWsSxX]\d{2}$'
+        RETURN a.uuid, a.naam, a.vplanNummer
+	    """
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
---
+++ Reports/Report0112.py
@@ -0,0 +1,21 @@
+from DQReport import DQReport
+
+
+class Report0106:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0112', title='Verkeersregelaars hebben een vplanDatum',
+                               spreadsheet_id='1701KVEMJAco3NPxfgOtHP0byI9_MQ1yhb1EQ3ujvU9I', datasource='Neo4J',
+                               persistent_column='E')
+
+        self.report.result_query = """
+        // Verkeersregelaars hebben een vplanDatum
+        MATCH (a:Verkeersregelaar {isActief:TRUE}) 
+        WHERE a.vplanDatum is null
+        RETURN a.uuid, a.naam, a.vplanNummer, a.vplanDatum
+	    """
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
