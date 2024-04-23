ea3bc2ccc4228848a870b0728dc831e29641de48
VerdoodtDries <dries@nordend.eu>
2024-04-23 10:44:46 +0200

    Replace query by a more performant one. Shift persistent column to column D.

--- Reports/Report0109.py
+++ Reports/Report0109.py
@@ -8,36 +8,30 @@ class Report0109:
     def init_report(self):
         self.report = DQReport(name='report0109', title='Geometrie is geldig: geen opeenvolgende punten',
                                spreadsheet_id='1AmtcjAkh5H95O_lXtd4p_MHeoqFpQKQlIRfQk3MxGQ4', datasource='PostGIS',
-                               persistent_column='E')
+                               persistent_column='D')
 
         self.report.result_query = """
-        with cte_geom as (
-            SELECT
-                assetuuid
-                , wkt_string
-                , SUBSTRING("wkt_string" FROM '^(POINT Z|LINESTRING Z|POLYGON Z)') AS wkt_string_prefix
-                , st_geomfromtext(wkt_string) as geom
-            FROM geometrie
-            WHERE 
-                wkt_string IS NOT NULL
-            --limit 100000
-        )
-        SELECT
-            assetuuid
-            , wkt_string
-        from cte_geom
-        where 
-            EXISTS (
-                SELECT 1
-                FROM (
-                    SELECT
-                        ST_Equals(ST_PointN(geom, i), ST_PointN(geom, i + 1)) AS is_duplicate
-                    FROM
-                        generate_series(1, ST_NumPoints(geom) - 1) AS i
-                ) AS subquery
-                WHERE is_duplicate
+            WITH cte_geom AS (
+                SELECT
+                    assetuuid,
+                    wkt_string,
+                    SUBSTRING("wkt_string" FROM '^[^ (]+') AS wkt_string_prefix,
+                    st_geomfromtext(wkt_string) AS geom
+                FROM
+                    geometrie
+                WHERE
+                    wkt_string IS NOT NULL
+                LIMIT 1000000
             )
-	"""
+            select distinct
+                assetuuid,
+                wkt_string_prefix,
+                wkt_string
+            FROM
+                cte_geom
+            where
+                ST_NPoints(st_removerepeatedpoints(geom)) <> ST_NPoints(geom)
+        	"""
 
     def run_report(self, sender):
         self.report.run_report(sender=sender)
