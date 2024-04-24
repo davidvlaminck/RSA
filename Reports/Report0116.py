ddced5064b5fa4c959cb39423aecc75bac043efa
VerdoodtDries <dries@nordend.eu>
2024-04-24 17:23:09 +0200

    Update the query: Switch and rename columns, acd column typeURI, sort by typeURI. Open by default in EM-infra.

--- Reports/Report0116.py
+++ Reports/Report0116.py
@@ -10,18 +10,21 @@ class Report0116:
                                title='De naam van de assets (DNBHoogSpanning of DNBLaagSpanning) is gelijk aan het eerste deel van het naampad van de installatie',
                                spreadsheet_id='11tU64Fc9Xh1IIlmufZ5yJ9Q99Sw4oP1kzCO0LrMbOn0',
                                datasource='Neo4J',
-                               persistent_column='F')
+                               persistent_column='F',
+                               link_type='eminfra'
+                               )
 
         self.report.result_query = """
         //De naam van de assets (DNBHoogSpanning of DNBLaagSpanning) is gelijk aan het eerste deel van het naampad van de installatie
         MATCH (a:DNBHoogspanning|DNBLaagspanning {isActief:true}) -[r:HoortBij {isActief:TRUE}]->(b:installatie {isActief:true})
         WHERE NOT a.naam = head(split(b.naampad, '/'))
         RETURN
-            a.uuid as otl_uuid
-            , a.naam as otl_naam
-            , b.uuid as legacy_uuid
-            , b.naam as legacy_naam
+            b.uuid as uuid
+            , b.typeURI as typeURI
             , b.naampad as legacy_naampad
+            , a.uuid as otl_uuid
+            , a.naam as otl_naam
+        ORDER BY typeURI, uuid
 	    """
 
     def run_report(self, sender):
