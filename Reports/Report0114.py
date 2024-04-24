59138b45d0f4827b9155933175493642364ae998
VerdoodtDries <dries@nordend.eu>
2024-04-24 15:02:11 +0200

    Update the query. Open by default in EM-infra. Update persistent column

--- Reports/Report0114.py
+++ Reports/Report0114.py
@@ -10,16 +10,15 @@ class Report0114:
                                title='EAN Nummer niet als commentaar gedocumenteerd',
                                spreadsheet_id='188xxFUa1uZ8GPgwB9a2c0gtkrdtacg5ft5TkODanatk',
                                datasource='Neo4J',
-                               persistent_column='I')
+                               persistent_column='F',
+                               link_type='eminfra')
 
         self.report.result_query = """
         // EAN wordt gecontroleerd door een regex match met 18 digits, starting with 54
-        MATCH (a {isActief:true})-[r:HoortBij {isActief:true}]->(b {isActief:true})
+        MATCH (a {isActief:true})
         where
-            a.notitie =~ '54\d{16}'
-            OR
-            b.notitie =~ '54\d{16}'
-        RETURN a.assetIdUri, a.uuid, a.notitie, a.eanNummer, b.assetIdUri, b.uuid, b.eanNummer, b.notitie
+            a.notitie =~ '^.*54\d{16}.*$'
+        RETURN a.uuid, a.notitie, a.naampad, a.typeURI, a.isActief
 	    """
 
     def run_report(self, sender):
