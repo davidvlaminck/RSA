1665022301d84d05dad83f9212cf9c09093038a3
VerdoodtDries <dries@nordend.eu>
2024-04-24 16:04:13 +0200

    Update the query. Open by default in EM-infra. Update persistent column. Append eanNummer van bijHorende OTL-asset (DNBHoogspanning of DNBLaagspanning)

--- Reports/Report0114.py
+++ Reports/Report0114.py
@@ -10,7 +10,7 @@ class Report0114:
                                title='EAN Nummer niet als commentaar gedocumenteerd',
                                spreadsheet_id='188xxFUa1uZ8GPgwB9a2c0gtkrdtacg5ft5TkODanatk',
                                datasource='Neo4J',
-                               persistent_column='G',
+                               persistent_column='H',
                                link_type='eminfra')
 
         self.report.result_query = """
@@ -21,8 +21,9 @@ class Report0114:
         OPTIONAL MATCH (a {isActief:true})-[:HoortBij]-(b:DNBHoogspanning|DNBLaagspanning)
         where
             a.notitie =~ '^.*54\d{16}.*$'
-        RETURN a.uuid as uuid, a.notitie as commentaar, a.naampad as naampad, a.typeURI as typeURI, a.isActief as isActief, b.eanNummer as eanNummer_bijhorendeAsset
-	    """
+        RETURN a.uuid as uuid, a.naampad as naampad, a.typeURI as typeURI, a.isActief as isActief, a.toestand as toestand, a.notitie as commentaar, b.eanNummer as eanNummer_bijhorendeAsset
+        ORDER BY typeURI, uuid
+        """
 
     def run_report(self, sender):
         self.report.run_report(sender=sender)
