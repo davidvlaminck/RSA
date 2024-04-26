0196352ad823052943a20c88a5adda681a0af105
VerdoodtDries <dries@nordend.eu>
2024-04-26 14:24:09 +0200

    update query to a more performant one

--- Reports/Report0121.py
+++ Reports/Report0121.py
@@ -14,47 +14,31 @@ class Report0121:
                                link_type='eminfra_onderdeel')
 
         self.report.result_query = """
-            with cte_netwerkpoort_aantal as (
-                select a.naam, unnest(array_agg(a.uuid)) as uuid, count(a.naam) as aantal 
-                from assets a 
-                where
-                    a.assettype = '6b3dba37-7b73-4346-a264-f4fe5b796c02' -- Netwerkpoort
-                    and 
-                    a.actief = true 
-                group by a.naam
-                having count(a.naam) > 1
-                order by naam asc
-            )
-            , cte_attribuutwaarden_netwerkpoort_type as (
-                select distinct *
-                from attribuutwaarden
-                where attribuutuuid in ('97977fa6-732b-4222-94fa-b9723945bd79') -- Netwerkpoort.type
-            )
-            , cte_attribuutwaarden_netwerkpoort_merk as (
-                select distinct *
-                from attribuutwaarden
-                where attribuutuuid in ('6db57eb2-e3ea-461f-812b-2d8fae7c2b0f') --Netwerkpoort.merk
-            )
-            select
-                a."uuid"
-                , a.naam
-                , a_aantal.aantal as dubbele_naam_aantal
-                , attr_type.waarde as type
-                , attr_merk.waarde as merk
-                , b.edeltadossiernummer as bestek_dossiernummer
-                , b.edeltabesteknummer  as bestel_besteknummer
-                , b.aannemernaam as bestek_aannemernaam
-            from cte_netwerkpoort_aantal a_aantal
-            left join assets a on a_aantal.uuid = a."uuid"
-            left join cte_attribuutwaarden_netwerkpoort_type attr_type on a."uuid" = attr_type.assetuuid
-            left join cte_attribuutwaarden_netwerkpoort_merk attr_merk on a."uuid" = attr_merk.assetuuid
-            left join bestekkoppelingen bk on a.uuid = bk.assetuuid 
-            left join bestekken b on bk.bestekuuid = b."uuid"
-            where
-                a.actief = true
-                and
-                a.assettype = '6b3dba37-7b73-4346-a264-f4fe5b796c02' -- Netwerkpoort
-            order by a.naam
+                    WITH dubbele_namen AS (
+            SELECT a.naam, count(*) AS aantal          
+            FROM assets a 
+            WHERE a.assettype = '6b3dba37-7b73-4346-a264-f4fe5b796c02' -- Netwerkpoort
+                AND a.actief = TRUE
+            GROUP BY a.naam
+            HAVING count(a.naam) > 1)
+        SELECT
+            a.uuid
+            , a.naam
+            , dubbele_namen.aantal
+            , a2.waarde AS "type"
+            , a3.waarde AS merk
+            , b.edeltadossiernummer AS bestek_dossiernummer
+            , b.edeltabesteknummer  AS bestel_besteknummer
+            , b.aannemernaam AS bestek_aannemernaam
+        FROM assets a
+            INNER JOIN dubbele_namen ON dubbele_namen.naam = a.naam
+            LEFT JOIN attribuutwaarden a2 ON a.uuid = a2.assetuuid AND a2.attribuutuuid = '97977fa6-732b-4222-94fa-b9723945bd79' -- Netwerkpoort.type
+            LEFT JOIN attribuutwaarden a3 ON a.uuid = a3.assetuuid AND a3.attribuutuuid = '6db57eb2-e3ea-461f-812b-2d8fae7c2b0f' -- Netwerkpoort.merk	
+            LEFT JOIN bestekkoppelingen bk ON a.uuid = bk.assetuuid 
+            LEFT JOIN bestekken b ON bk.bestekuuid = b.uuid
+        WHERE a.actief = TRUE AND a.assettype = '6b3dba37-7b73-4346-a264-f4fe5b796c02' -- Netwerkpoort
+        ORDER BY a.naam;
+
             """
 
     def run_report(self, sender):
