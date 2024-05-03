a8c640f433a1616685736bd57dbbe1ae3e3a4ec1
VerdoodtDries <dries@nordend.eu>
2024-05-03 10:20:41 +0200

    Update query: filter netwerkelement met attribuutwaarde 'l2-switch'.

--- Reports/Report0031.py
+++ Reports/Report0031.py
@@ -10,22 +10,29 @@ class Report0031:
                                title="Netwerkelementen met gebruik 'L2-switch' hebben een hoortbij relatie naar installatie L2AccesStructuur",
                                spreadsheet_id='1k5uUwLmf5IFVhftY7klBmylWtuEBwP8URjYPMcAB71w',
                                datasource='PostGIS',
-                               persistent_column='C')
+                               persistent_column='D')
 
         self.report.result_query = """
-WITH elementen AS (
-	SELECT * 
-	FROM assets 
-	WHERE assettype = 'b6f86b8d-543d-4525-8458-36b498333416' AND actief = TRUE), -- Netwerkelement
-relaties AS (
-	SELECT assetrelaties.* 
-	FROM assetrelaties
-		LEFT JOIN assets ON assets.uuid = doeluuid AND assets.actief = TRUE AND assets.assettype = '30b571a9-77ef-4289-a458-139882a5e97a' -- L2AccessStructuur
-	WHERE relatietype = '812dd4f3-c34e-43d1-88f1-3bcd0b1e89c2') -- HoortBij
-SELECT elementen.uuid, elementen.naam  
-FROM elementen
-	LEFT JOIN relaties ON elementen.uuid = relaties.bronuuid
-WHERE relaties.uuid IS NULL;"""
-
+            -- Netwerkementen met gebruik 'L2-switch' hebben een hoortbij relatie naar installatie L2AccesStructuur
+            WITH elementen AS (
+                SELECT a.*, aw.waarde as gebruik 
+                FROM assets a
+                left join attribuutwaarden aw on a.uuid = aw.assetuuid and aw.attribuutuuid = '63ac54e9-8ea4-4e0f-8c5d-d7891a7af811' -- attribuut gebruik
+                where
+                    a.assettype = 'b6f86b8d-543d-4525-8458-36b498333416' -- Netwerkelement
+                    AND a.actief = true 
+                    and	aw.waarde like '%/l2-switch' -- attribuutwaarde gebruik komt overeen met l2-switch (einde van de URI)
+                ), 
+            relaties AS (
+                SELECT assetrelaties.*
+                FROM assetrelaties
+                    LEFT JOIN assets ON assets.uuid = doeluuid AND assets.actief = TRUE AND assets.assettype = '30b571a9-77ef-4289-a458-139882a5e97a' -- L2AccessStructuur
+                    left join relatietypes on assetrelaties.relatietype = relatietypes."uuid" 
+                WHERE relatietype = '812dd4f3-c34e-43d1-88f1-3bcd0b1e89c2') -- HoortBij
+            SELECT elementen.uuid, elementen.naam, elementen.gebruik
+            FROM elementen
+            LEFT JOIN relaties ON elementen.uuid = relaties.bronuuid
+            WHERE relaties.uuid IS NULL;  -- Hoortbij Relatie ontbreekt
+            """
     def run_report(self, sender):
         self.report.run_report(sender=sender)
