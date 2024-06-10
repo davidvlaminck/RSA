7ca5bb2d8fe8f1a068351bdb911f3e025d770a97
VerdoodtDries <dries@nordend.eu>
2024-06-10 12:00:40 +0200

    Toevoegen geometrie rapporten i.v.m. identieke eindpunten van een Linestring.

---
+++ Reports/Report0130.py
@@ -0,0 +1,42 @@
+from DQReport import DQReport
+
+
+class Report0130:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0130',
+                               title='Geometrie Linestring heeft identieke eindpunten en bevat meer dan 2 punten',
+                               spreadsheet_id='1sbY-T8UZqE-tau7Hu6860OLXjNQ0wuDC5AI-yov35NY',
+                               datasource='PostGIS',
+                               persistent_column='E'
+                               )
+
+        self.report.result_query = """
+            with cte_asset_of_interest as (
+                select
+                    a.uuid
+                    , at.label as assettype_label
+                    , st_geomfromtext(g.wkt_string) as geom
+                    , st_npoints(st_geomfromtext(g.wkt_string)) as npoints 
+                from assets a
+                left join geometrie g on a.uuid = g.assetuuid
+                left join assettypes at on a.assettype = at.uuid
+                where g.wkt_string ~ '^LINESTRING*'
+            )
+            select
+                uuid
+                , assettype_label
+                , left(st_astext(geom), 100) as wkt_geom_afgerond
+                , npoints
+            from cte_asset_of_interest
+            where
+                npoints > 2 -- Linestring consists of more than 2 points
+                and
+                st_pointn(geom, 1) = st_pointn(geom, -1) -- Endpoints of the linestring are identical
+            order by assettype_label asc
+            """
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
