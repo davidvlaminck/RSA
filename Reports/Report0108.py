331a7830ad80b871990a8d4431b0c19836e18876
VerdoodtDries <dries@nordend.eu>
2024-06-11 13:08:41 +0200

    Aanpassing query: Filter multiparts op basis van wkt_string bevat regular expression 'MULTI'. Toevoegen extra kolom: aantal_delen.

--- Reports/Report0108.py
+++ Reports/Report0108.py
@@ -8,7 +8,7 @@ class Report0108:
     def init_report(self):
         self.report = DQReport(name='report0108', title='Geometrie is geldig: enkelvoudig punt, lijn of vlak',
                                spreadsheet_id='1glkACdbjMyh81DFyxuKpC1yNjDZeWjDH_kxAyw-TkY0', datasource='PostGIS',
-                               persistent_column='E')
+                               persistent_column='F')
 
         self.report.result_query = """
         with cte_geom as (
@@ -24,18 +24,21 @@ class Report0108:
         select
             assetuuid
             , st_geometrytype(geom) as geometry_type
+            , geom
         from cte_geom
-        where st_numgeometries(geom) > 1
+        where wkt_string ~ '^MULTI.*$' 
         )
         select 
             g.assetuuid
             , g.geometry_type
+            , st_numgeometries(geom) as aantal_delen 
             , at.naam
             , at.URI
         from cte_geom_multiparts g
         LEFT JOIN assets a ON g.assetuuid = a.uuid
         LEFT JOIN assettypes at ON a.assettype = at.uuid
         where at.URI !~ '^(https://grp.).*' -- Regular expression does not start with 
+        order by at.uri;
         """
 
     def run_report(self, sender):
