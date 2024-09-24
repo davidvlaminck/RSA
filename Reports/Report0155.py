de9e7084a4dda4b9cf93a0fe6c9e19f24947e3ea
VerdoodtDries <dries@nordend.eu>
2024-09-24 11:59:32 +0200

    Edit report 0155. Add filter isActief and append 2 columns isActief.

--- Reports/Report0155.py
+++ Reports/Report0155.py
@@ -10,13 +10,14 @@ class Report0155:
                                title='LS/HS met Voedingsrelatie anders dan LS-Deel/HS-Deel',
                                spreadsheet_id='19jhMsSzcxBHTjveVWMJWXRlKxYKlw1r0m5j5JSMfJ4k',
                                datasource='Neo4J',
-                               persistent_column='I'
+                               persistent_column='K'
                                )
 
         self.report.result_query = """
         // Obtain all LS and HS nodes with Voeding relationship to nodes that are not LSDeel or HSDeel respectively
         MATCH (n)-[r:Voedt]-(other)
         WHERE 
+            n.isActief = true AND
             (n:LS AND NOT other:LSDeel) OR
             (n:HS AND NOT other:HSDeel)
         RETURN
@@ -24,10 +25,12 @@ class Report0155:
             , n.typeURI as typeURI
             , n.naampad as naampad
             , n.naam as naam
+            , n.isActief as isActief
             , other.uuid AS asset2_uuid
             , other.typeURI as asset2_typeURI
             , other.naampad AS asset2_naampad
             , other.naam as asset2_naam
+            , other.isActief as asset2_isActief
         ORDER BY other.typeURI asc
         """
 
