386bd0aede6af7a45fa5672e47ffa187a7935b24
VerdoodtDries <dries@nordend.eu>
2025-01-17 14:51:10 +0100

    Update Report0110. Use tables geometry en locatie en coalesce-function.

--- Reports/Report0110.py
+++ Reports/Report0110.py
@@ -11,27 +11,27 @@ class Report0110:
                                persistent_column='E')
 
         self.report.result_query = """
-            with cte_geom as (
-            SELECT
-                g.assetuuid
-                , at.uri
-                , g.wkt_string
-                , SUBSTRING("wkt_string" FROM '^(POINT Z|LINESTRING Z|POLYGON Z)') AS wkt_string_prefix
-                , st_geomfromtext(g.wkt_string) as geom
-            FROM geometrie g
-            left join assets a on g.assetuuid = a.uuid
-            left join assettypes at on a.assettype = at.uuid
-            WHERE 
-                g.wkt_string IS NOT NULL
-            --limit 100000
-            )
+            with cte_asset as (
+                        SELECT
+                            a.uuid
+                            , at.label as assettype
+                            , coalesce(g.wkt_string, l.geometrie) as wkt_string
+                            , coalesce(g.geometry, l.geometry) as geometry
+                        FROM assets a
+                        left join geometrie g on a.uuid = g.assetuuid
+                        left join locatie l on a.uuid = l.assetuuid
+                        left join assettypes at on a.assettype = at.uuid
+                        where
+                            a.actief is true
+                        )
             select
-                assetuuid
-                , uri
-                , 'invalid geometry: ' || reason(st_isvaliddetail(geom)) as info
-                , wkt_string_prefix
-            from cte_geom
-            where not st_isvalid(geom) 
+                uuid
+                , assettype
+                , 'invalid geometry: ' || reason(st_isvaliddetail(geometry)) as info
+                , SUBSTRING("wkt_string" FROM '^(POINT Z|LINESTRING Z|POLYGON Z)') AS wkt_string_prefix
+            from cte_asset
+            where
+            not st_isvalid(geometry)
 	        """
 
     def run_report(self, sender):
