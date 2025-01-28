4bdadcb619b24d3a21da3a6f53e1b6921910a30d
VerdoodtDries <dries@nordend.eu>
2025-01-28 14:50:22 +0100

    Update Report0039. Performance improvement.

--- Reports/Report0039.py
+++ Reports/Report0039.py
@@ -13,7 +13,7 @@ class Report0039:
                                convert_columns_to_numbers=['K', 'S', 'X', 'Y'])
 
         self.report.result_query = """
-WITH awegen ("aweg_ident8", "beginpositie", "eindpositie") AS ( VALUES
+WITH cte_awegen ("aweg_ident8", "beginpositie", "eindpositie") AS ( VALUES
 	('B1010001',0.000,5.000),
 	('B1010002',0.000,5.000),
 	('B4010001',0.000,2.338),
@@ -46,21 +46,46 @@ WITH awegen ("aweg_ident8", "beginpositie", "eindpositie") AS ( VALUES
 	('R0080961',0.000,0.212),
 	('R0082021',0.000,0.000),
 	('R0082051',0.160,0.160)),
-ruwe_data AS (
+cte_attribuutwaarden as (
+	select
+		assetuuid
+		, MAX(CASE WHEN attribuutuuid = '6e319c32-8e94-476c-965f-32b93c461d20' THEN waarde END) AS aanstuurstroomDriversInMa
+		, MAX(CASE WHEN attribuutuuid = '568ba32e-847c-496a-be20-5d022897f032' THEN waarde END) AS aantal_verlichtingstoestellen
+		, MAX(CASE WHEN attribuutuuid = '6e319c32-8e94-476c-965f-32b93c461d20' THEN waarde END) AS contractnummer_levering_LED
+		, MAX(CASE WHEN attribuutuuid = 'ed270590-4b11-421d-b921-36034323a9a9' THEN waarde END) AS datum_installatie_LED
+		, MAX(CASE WHEN attribuutuuid = '070149cc-55f4-491f-a034-21e832e3a9e5' THEN waarde END) AS lamp_type
+		, MAX(CASE WHEN attribuutuuid = 'e7ad2d9f-45f3-4e4a-be98-51d71e19c28b' THEN waarde END) AS LED_verlichting
+		, MAX(CASE WHEN attribuutuuid = '218f8269-21eb-445a-9c77-acb3faf6c3ba' THEN waarde END) AS lumen_pakket_LED
+		, MAX(CASE WHEN attribuutuuid = '8ea7f7ef-c187-4a68-a92b-6a0ca855ba50' THEN waarde END) AS verlichtingstoestel_systeemvermogen
+	from attribuutwaarden
+	where attribuutuuid in 
+		(
+			  '6f3d2728-448c-4975-8f4a-cba21d2634b8'  -- aanstuurstroomDriversInMa
+			, '568ba32e-847c-496a-be20-5d022897f032'  -- aantal_verlichtingstoestellen
+			, '6e319c32-8e94-476c-965f-32b93c461d20'  -- contractnummer_levering_LED
+			, 'ed270590-4b11-421d-b921-36034323a9a9'  -- datum_installatie_LED
+			, '070149cc-55f4-491f-a034-21e832e3a9e5'  -- lamp_type
+			, 'e7ad2d9f-45f3-4e4a-be98-51d71e19c28b'  -- LED_verlichting
+			, '218f8269-21eb-445a-9c77-acb3faf6c3ba'  -- lumen_pakket_LED
+			, '8ea7f7ef-c187-4a68-a92b-6a0ca855ba50'  -- verlichtingstoestel_systeemvermogen
+		)
+	group by assetuuid
+),
+cte_ruwe_data AS (
 	SELECT assets.uuid, assets.naampad, assets.toestand, assets.actief
-		, attribuutwaarden_aanstuurstroomDriversInMa.waarde AS aanstuurstroomDriversInMa
-		, attribuutwaarden_aantal_verlichtingstoestellen.waarde AS aantal_verlichtingstoestellen
-		, attribuutwaarden_contractnummer_levering_LED.waarde AS contractnummer_levering_LED
-		, attribuutwaarden_datum_installatie_LED.waarde AS datum_installatie_LED
-		, attribuutwaarden_lamp_type.waarde AS lamp_type
-		, attribuutwaarden_LED_verlichting.waarde AS LED_verlichting
-		, attribuutwaarden_lumen_pakket_LED.waarde AS lumen_pakket_LED
+		, aw.aanstuurstroomDriversInMa
+		, aw.aantal_verlichtingstoestellen
+		, aw.contractnummer_levering_LED
+		, aw.datum_installatie_LED
+		, aw.lamp_type
+		, aw.LED_verlichting
+		, aw.lumen_pakket_LED
+--		, aw.verlichtingstoestel_systeemvermogen
 		, locatie.ident8
 		, CASE WHEN referentiepaal_opschrift IS NOT NULL THEN
 			CASE WHEN referentiepaal_afstand IS NULL THEN referentiepaal_opschrift
 			ELSE referentiepaal_opschrift + referentiepaal_afstand / 1000.0 END
 		ELSE NULL END AS locatie_referentiepunt
-		-- , attribuutwaarden_verlichtingstoestel_systeemvermogen.waarde AS verlichtingstoestel_systeemvermogen
 		, 'POINT Z (' || locatie.x || ' ' || locatie.y || ' ' || coalesce(locatie.z, 0) || ')' as wkt_geom
 		, locatie.adres_gemeente
 		-- Overschrijf Brussel >> Vlaams-Brabant
@@ -70,25 +95,24 @@ ruwe_data AS (
 		end as adres_provincie
 		, locatie.adres_provincie as adres_provincie_origineel_met_brussel
 	FROM assets 
-		LEFT JOIN attribuutwaarden attribuutwaarden_aanstuurstroomDriversInMa ON assets.uuid = attribuutwaarden_aanstuurstroomDriversInMa.assetuuid AND attribuutwaarden_aanstuurstroomDriversInMa.attribuutuuid = '6f3d2728-448c-4975-8f4a-cba21d2634b8'
-		LEFT JOIN attribuutwaarden attribuutwaarden_aantal_verlichtingstoestellen ON assets.uuid = attribuutwaarden_aantal_verlichtingstoestellen.assetuuid AND attribuutwaarden_aantal_verlichtingstoestellen.attribuutuuid = '568ba32e-847c-496a-be20-5d022897f032'
-		LEFT JOIN attribuutwaarden attribuutwaarden_contractnummer_levering_LED ON assets.uuid = attribuutwaarden_contractnummer_levering_LED.assetuuid AND attribuutwaarden_contractnummer_levering_LED.attribuutuuid = '6e319c32-8e94-476c-965f-32b93c461d20'
-		LEFT JOIN attribuutwaarden attribuutwaarden_datum_installatie_LED ON assets.uuid = attribuutwaarden_datum_installatie_LED.assetuuid AND attribuutwaarden_datum_installatie_LED.attribuutuuid = 'ed270590-4b11-421d-b921-36034323a9a9'
-		LEFT JOIN attribuutwaarden attribuutwaarden_lamp_type ON assets.uuid = attribuutwaarden_lamp_type.assetuuid AND attribuutwaarden_lamp_type.attribuutuuid = '070149cc-55f4-491f-a034-21e832e3a9e5'
-		LEFT JOIN attribuutwaarden attribuutwaarden_LED_verlichting ON assets.uuid = attribuutwaarden_LED_verlichting.assetuuid AND attribuutwaarden_LED_verlichting.attribuutuuid = 'e7ad2d9f-45f3-4e4a-be98-51d71e19c28b'
-		LEFT JOIN attribuutwaarden attribuutwaarden_lumen_pakket_LED ON assets.uuid = attribuutwaarden_lumen_pakket_LED.assetuuid AND attribuutwaarden_lumen_pakket_LED.attribuutuuid = '218f8269-21eb-445a-9c77-acb3faf6c3ba'
-		-- LEFT JOIN attribuutwaarden attribuutwaarden_verlichtingstoestel_systeemvermogen ON assets.uuid = attribuutwaarden_verlichtingstoestel_systeemvermogen.assetuuid AND attribuutwaarden_verlichtingstoestel_systeemvermogen.attribuutuuid = '8ea7f7ef-c187-4a68-a92b-6a0ca855ba50'
+		LEFT JOIN cte_attribuutwaarden aw ON assets.uuid = aw.assetuuid
 		LEFT JOIN locatie ON assets.uuid = locatie.assetuuid 
-	WHERE assettype = '4dfad588-277c-480f-8cdc-0889cfaf9c78' AND assets.actief = true
+	where
+		assettype = '4dfad588-277c-480f-8cdc-0889cfaf9c78'
+		AND assets.actief = true
 	),
 opkuis1 AS (
-	SELECT ruwe_data.*,
+	select
+		cte_ruwe_data.*,
 		CASE WHEN aanstuurstroomDriversInMa IS NOT NULL AND lamp_type <> 'LED' THEN 'LED'
 			WHEN lumen_pakket_LED IS NOT NULL AND lumen_pakket_LED <> '0' THEN 'LED'
 			ELSE lamp_type END AS lamp_type_opgekuist,
 		CASE WHEN aantal_verlichtingstoestellen IS NOT NULL THEN aantal_verlichtingstoestellen::NUMERIC
-			ELSE 1::NUMERIC END AS aantal_verlichtingstoestellen_getal
-	FROM ruwe_data),
+			ELSE 1::NUMERIC END AS aantal_verlichtingstoestellen_getal,
+		cte_awegen.*		
+	FROM cte_ruwe_data
+	LEFT JOIN cte_awegen ON ident8 = cte_awegen.aweg_ident8 AND locatie_referentiepunt between cte_awegen.beginpositie AND cte_awegen.eindpositie
+),
 opkuis2 AS (
 	SELECT *, 
 		CASE WHEN lamp_type_opgekuist = 'LED' THEN 'True' 
@@ -116,9 +140,7 @@ opkuis2 AS (
 			WHEN lamp_type_opgekuist = 'NaLP91' THEN 91
 		ELSE 0 END AS vermogen
 	FROM opkuis1
-	LEFT JOIN awegen ON awegen.aweg_ident8 = opkuis1.ident8 AND awegen.beginpositie <= locatie_referentiepunt AND opkuis1.locatie_referentiepunt <= awegen.eindpositie
 	),
--- SELECT DISTINCT lamp_type_opgekuist, vermogen FROM opkuis2; -- mappingtabel vermogens
 opkuis3 AS (
 	SELECT *, 
 		CASE WHEN lamp_type_opgekuist = 'LED' THEN vermogen ELSE vermogen*1.15 END AS vermogen_inc_verlies,
@@ -137,7 +159,6 @@ opkuis3 AS (
 			ELSE 'locatie ongekend'
 		END AS wegcategorie
 	FROM opkuis2)
--- SELECT DISTINCT lamp_type_opgekuist, beperkte_benaming FROM opkuis3; -- mappingtabel beperkte_benaming
 SELECT * FROM opkuis3;
 """
 
