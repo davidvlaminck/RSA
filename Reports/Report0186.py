69ec16bf3e3529cd9df8635c53fb86c27e701c0a
VerdoodtDries <dries@nordend.eu>
2025-04-10 15:52:07 +0200

    update query

--- Reports/Report0186.py
+++ Reports/Report0186.py
@@ -45,20 +45,23 @@ class Report0186:
             a1.uuid
             , a1.assettype_naam
             , a1.naam
-            , a1.commentaar
-            , a1.bestek_startdatum
-            , a1.bestek_einddatum
-            , a1.edeltadossiernummer
+            , a1.geometry
+            , a2.uuid
+            , a2.assettype_naam
+            , a2.geometry
+            , st_distance(a1.geometry, a2.geometry) as afstand_tussen_verwerkingseenheid_en_demodulator
+        --    , a1.bestek_startdatum
+        --    , a1.bestek_einddatum
+        --    , a1.edeltadossiernummer
             , a1.edeltabesteknummer
             , a1.aannemernaam
-            , a1.geometry
             , replace(aw.waarde, '''', '"')::json->>'DtcAssetVersie.context' as assetVersie_context
             , replace(aw.waarde, '''', '"')::json->>'DtcAssetVersie.versienummer' as assetVersie_versienummer
             , replace(aw.waarde, '''', '"')::json->>'DtcAssetVersie.timestamp' as assetVersie_timestamp
         from cte_assets_PT a1
         left join attribuutwaarden aw on a1.uuid = aw.assetuuid and aw.attribuutuuid = 'b4a9a588-5643-44e6-b4d4-b8cdd6685f79' --AssetVersie
         left join assetrelaties rel on a1.uuid = rel.bronuuid and rel.relatietype = '3ff9bf1c-d852-442e-a044-6200fe064b20' -- Bevestiging
-        left join cte_assets_PT a2 on rel.doeluuid = a2.uuid and a2.assettype = 'ba1e055e-6a83-4a1a-b613-5e93ecb0baab'
+        left join cte_assets_PT a2 on st_dwithin(a1.geometry, a2.geometry, 100) and a2.assettype = 'ba1e055e-6a83-4a1a-b613-5e93ecb0baab' -- PT-demodulatoren
         where
             a1.assettype = '0390242b-4462-436b-bcfa-035f2ee72130' -- PT-verwerkingseenheid
             and 
