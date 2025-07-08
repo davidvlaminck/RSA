106ca1853fe9045a2f5b6ff50024e412e2fff5c6
VerdoodtDries <dries@nordend.eu>
2025-07-08 14:17:55 +0200

    Update RSA Report0195

--- Reports/Report0195.py
+++ Reports/Report0195.py
@@ -19,9 +19,12 @@ class Report0195:
             , a.toestand
             , a.actief
             , a.naam
+            , a.commentaar 
+            , i.gebruikersnaam
             , l.geometry
         from assets a
         left join locatie l on a."uuid" = l.assetuuid
+        left join identiteiten i on a.toezichter = i."uuid" 
         where
             a.assettype = '10377658-776f-4c21-a294-6c740b9f655e' -- Kast (Legacy)
             and
@@ -29,7 +32,7 @@ class Report0195:
             and
             l.geometry is null
         order by
-            a.toestand
+            a.toestand, i.gebruikersnaam 
         """
 
     def run_report(self, sender):
