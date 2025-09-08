620077a65aed431330408a1f67d1c76d92fc2b96
VerdoodtDries <dries@nordend.eu>
2025-09-08 15:39:35 +0200

    Add Report0199. Seinbruggen

---
+++ Reports/Report0199.py
@@ -0,0 +1,41 @@
+from DQReport import DQReport
+
+
+class Report0199:
+    def __init__(self):
+        self.report = None
+
+    def init_report(self):
+        self.report = DQReport(name='report0199',
+                               title='Seinbrug is een punt',
+                               spreadsheet_id='',
+                               datasource='PostGIS',
+                               persistent_column='E',
+                               link_type='eminfra')
+
+        self.report.result_query = """
+            with cte_seinbrug as (
+                select
+                    a.uuid
+                    , a.naam
+                    , g.geometry
+                    , st_geometrytype(g.geometry) as "geometry_type"
+                    , ST_area(g.geometry) as "oppervlakte"	
+                from assets a
+                left join geometrie g on a."uuid" = g.assetuuid 
+                where a.assettype = '40b2e487-f4b8-48a2-be9d-e68263bab75a'  -- Seinbrug
+                 and a.actief is true
+                order by st_area(g.geometry) desc
+            )
+            select
+                uuid, naam, geometry, oppervlakte
+            from cte_seinbrug
+            where
+                geometry_type = 'ST_Point'
+                or
+                geometry_type = 'ST_Polygon' and oppervlakte <= 4
+            order by oppervlakte desc
+            """
+
+    def run_report(self, sender):
+        self.report.run_report(sender=sender)
