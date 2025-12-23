2546a1c6ac12d43fd1fee7640f9a22a352a5838a
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2025-12-23 11:10:10 +0100

    added possible solution, needs testing

--- Reports/Report0059.py
+++ Reports/Report0059.py
@@ -21,3 +21,39 @@ class Report0059:
 
     def run_report(self, sender):
         self.report.run_report(sender=sender)
+
+
+# to verify
+aql_query = """
+LET voedt_key = FIRST(FOR rt IN relatietypes FILTER rt.short == "Voedt" LIMIT 1 RETURN rt._key)
+LET upsl_key  = FIRST(FOR at IN assettypes FILTER at.short_uri == "onderdeel#UPSLegacy" LIMIT 1 RETURN at._key)
+
+
+FOR x IN assets
+  FILTER x.AIMDBStatus_isActief == true
+  
+  FOR v, e, p IN 1..10 ANY x assetrelaties
+    OPTIONS { order: "bfs", uniqueVertices: "global", uniqueEdges: "path" }
+    FILTER
+      e.relatietype_key == voedt_key
+      AND v._id == x._id
+      AND (
+        FOR n IN p.vertices
+          FILTER n.assettype_key == upsl_key
+          LIMIT 1
+          RETURN 1
+      ) == []
+
+    LET path_loop = (
+      FOR n IN p.vertices
+        RETURN [n._key, n.typeURI]
+    )
+
+    RETURN DISTINCT {
+      uuid: x._key,
+      naampad: x.AIMNaamObject_naampad,
+      typeURI: x.typeURI,
+      toestand: x.toestand,
+      path_loop: path_loop
+    }
+"""
