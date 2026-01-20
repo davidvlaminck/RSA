8ec6e1942e5cbdc1d8af6b71a10c45a6bae6cee0
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-01-20 14:24:37 +0100

    AQL query added

--- Reports/Report0155.py
+++ Reports/Report0155.py
@@ -36,3 +36,44 @@ class Report0155:
 
     def run_report(self, sender):
         self.report.run_report(sender=sender)
+
+
+# AQL equivalent (documentation / future migration)
+# Cypher:
+# MATCH (n)-[r:Voedt]-(other)
+# WHERE n.isActief = true AND ((n:LS AND NOT other:LSDeel) OR (n:HS AND NOT other:HSDeel))
+# RETURN n..., other...
+
+aql_query = """
+LET ls_key     = FIRST(FOR at IN assettypes FILTER at.short_uri == "lgc:installatie#LS"            LIMIT 1 RETURN at._key)
+LET hs_key     = FIRST(FOR at IN assettypes FILTER at.short_uri == "lgc:installatie#HS"            LIMIT 1 RETURN at._key)
+LET lsdeel_key = FIRST(FOR at IN assettypes FILTER at.short_uri == "lgc:installatie#LSDeel"        LIMIT 1 RETURN at._key)
+LET hsdeel_key = FIRST(FOR at IN assettypes FILTER at.short_uri == "lgc:installatie#HSDeel"        LIMIT 1 RETURN at._key)
+
+FOR n IN assets
+  FILTER n.AIMDBStatus_isActief == true
+  FILTER n.assettype_key IN [ls_key, hs_key]
+
+  FOR other, rel IN ANY n voedt_relaties
+
+    FILTER (
+      (n.assettype_key == ls_key AND other.assettype_key != lsdeel_key)
+      OR
+      (n.assettype_key == hs_key AND other.assettype_key != hsdeel_key)
+    )
+
+    SORT other['@type'] ASC
+
+    RETURN {
+      uuid: n._key,
+      typeURI: n['@type'],
+      naampad: n.NaampadObject_naampad,
+      naam: n.AIMNaamObject_naam,
+      isActief: n.AIMDBStatus_isActief,
+      asset2_uuid: other._key,
+      asset2_typeURI: other['@type'],
+      asset2_naampad: other.NaampadObject_naampad,
+      asset2_naam: other.AIMNaamObject_naam,
+      asset2_isActief: other.AIMDBStatus_isActief
+    }
+"""
