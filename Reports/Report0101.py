28516bd898b9f8e9dd195d03e447117b487e56f5
VerdoodtDries <dries@nordend.eu>
2026-02-19 16:12:02 +0100

    Update SQL query (readability)

--- Reports/Report0101.py
+++ Reports/Report0101.py
@@ -13,31 +13,51 @@ class Report0101(BaseReport):
                                recalculate_cells=[('Dataconflicten', 'A1'), ('>10 jaar oud', 'A1')])
 
         self.report.result_query = """
-WITH vrs AS (
-    SELECT assets.*, ST_SETSRID(ST_MakePoint(cast(x as float), cast(y as float)), 31370) AS geom
-    FROM assets 
-        LEFT JOIN locatie ON assets.uuid = locatie.assetuuid
-    WHERE assettype IN ('13fa9473-f919-432a-bd00-bc19645bd30a','40f86745-ecaa-456b-8262-0a1f014602df'))
-SELECT vrs.uuid as em_infra_link, split_part(naampad, '/', 1) AS installatie, actief, toestand, ST_X(ST_Transform(geom, 4326)) AS longitude, ST_Y(ST_Transform(geom, 4326)) AS latitude
-    , locatie.adres_gemeente, locatie.adres_provincie, text(date(indienstdatum)) AS indienstdatum
-    , CASE WHEN uitdienstdatum IS NULL AND indienstdatum IS NOT NULL AND indienstdatum <= CURRENT_DATE THEN text('in dienst')
-    	WHEN indienstdatum IS NOT NULL AND indienstdatum <= CURRENT_DATE AND uitdienstdatum IS NOT NULL AND CURRENT_DATE < uitdienstdatum THEN text('in dienst')
-    	ELSE to_char(date(uitdienstdatum), 'yyyy-mm-dd') END AS uitdienstdatum
-    , vplannummer AS vplan_nr, left(vplannummer, 7) AS vplan_kort, vplan_koppelingen.commentaar AS commentaar
-    , bestekken.edeltadossiernummer, bestekken.aannemernaam
-    , CASE WHEN uitdienstdatum IS NULL AND indienstdatum <= CURRENT_DATE - INTERVAL '10' YEAR THEN TRUE ELSE FALSE END AS "10_jaar_oud"
-    , CASE WHEN vplan_koppelingen.uuid IS NULL AND actief = TRUE AND toestand = 'in-gebruik' THEN TRUE
-        WHEN uitdienstdatum IS NULL AND vplan_koppelingen.uuid IS NOT NULL AND actief = TRUE AND toestand <> 'in-gebruik' AND toestand <> 'overgedragen' THEN TRUE
-        WHEN locatie.adres_provincie IS NULL OR locatie.x IS NULL AND actief = TRUE THEN TRUE
-        ELSE FALSE END AS dataconflicten
-    , naampad
-FROM vrs
-    LEFT JOIN locatie ON vrs.uuid = locatie.assetuuid
-    LEFT JOIN vplan_koppelingen ON vrs.uuid = vplan_koppelingen.assetuuid
-    LEFT JOIN bestekkoppelingen ON vrs.uuid = bestekkoppelingen.assetuuid AND lower(bestekkoppelingen.koppelingstatus) = 'actief'
-    LEFT JOIN bestekken ON bestekkoppelingen.bestekuuid = bestekken.uuid
-WHERE (vplan_koppelingen.uuid IS NOT NULL AND actief = FALSE) OR actief = TRUE
-ORDER BY actief DESC, dataconflicten, naampad;"""
+            WITH vrs AS (
+                SELECT
+                    a.*,
+                    l.geometry AS geom
+                FROM assets a
+                LEFT JOIN locatie l ON a.uuid = l.assetuuid
+                WHERE a.assettype IN (
+                    '13fa9473-f919-432a-bd00-bc19645bd30a',  -- Verkeersregelaar (Legacy)
+                    '40f86745-ecaa-456b-8262-0a1f014602df'  -- ITSApp-RIS (Legacy)
+                )
+            )
+            SELECT
+                v.uuid,
+                split_part(v.naampad, '/', 1) AS installatie,
+                v.naampad,
+                v.actief,
+                v.toestand,
+                l.adres_gemeente,
+                l.adres_provincie,
+                to_char(date(vplan.indienstdatum), 'yyyy-mm-dd') AS indienstdatum,
+                CASE
+                    WHEN vplan.uitdienstdatum IS NULL AND vplan.indienstdatum IS NOT NULL AND vplan.indienstdatum <= CURRENT_DATE THEN 'in dienst'
+                    WHEN vplan.indienstdatum IS NOT NULL AND vplan.uitdienstdatum IS NOT NULL AND CURRENT_DATE BETWEEN vplan.indienstdatum AND vplan.uitdienstdatum THEN 'in dienst'
+                    ELSE to_char(date(vplan.uitdienstdatum), 'yyyy-mm-dd')
+                END AS uitdienstdatum,
+                vplan.vplannummer AS vplan_nr,
+                left(vplan.vplannummer, 7) AS vplan_nr_kort,
+                vplan.commentaar,
+                b.edeltadossiernummer,
+                b.aannemernaam,
+                (vplan.uitdienstdatum IS NULL AND vplan.indienstdatum <= CURRENT_DATE - INTERVAL '10 YEAR') AS "10_jaar_oud",
+                CASE
+                    WHEN vplan.uuid IS NULL AND v.actief = TRUE AND v.toestand = 'in-gebruik' THEN TRUE
+                    WHEN vplan.uitdienstdatum IS NULL AND vplan.uuid IS NOT NULL AND v.actief = TRUE AND v.toestand NOT IN ('in-gebruik', 'overgedragen') THEN TRUE
+                    WHEN (l.adres_provincie IS NULL OR l.geometry IS NULL) AND v.actief = TRUE THEN TRUE
+                    ELSE FALSE
+                END AS dataconflicten
+            FROM vrs v
+            LEFT JOIN locatie l ON v.uuid = l.assetuuid
+            LEFT JOIN vplan_koppelingen vplan ON v.uuid = vplan.assetuuid
+            LEFT JOIN bestekkoppelingen bk ON v.uuid = bk.assetuuid AND lower(bk.koppelingstatus) = 'actief'
+            LEFT JOIN bestekken b ON bk.bestekuuid = b.uuid
+            WHERE (vplan.uuid IS NOT NULL AND v.actief = FALSE) OR v.actief = TRUE
+            ORDER BY v.actief DESC, dataconflicten, v.naampad, uitdienstdatum DESC;
+            """
 
     def run_report(self, sender) -> None:
         self.report.run_report(sender=sender)
