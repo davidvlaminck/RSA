568ddb28b792b0de56ba66095d6ca5c41bdf03f7
VerdoodtDries <dries@nordend.eu>
2026-02-20 09:24:35 +0100

    Update AQL query Report0101

--- Reports/Report0101.py
+++ Reports/Report0101.py
@@ -81,7 +81,9 @@ LET candidates = (
       naam: a.AIMNaamObject_naam,
       naampad: a.NaampadObject_naampad,
       naampad_parent: a.naampad_parent,
-      toestand: a.toestand
+      toestand: a.toestand,
+      loc: a.loc,
+      bs: a.bs,
     }
 )
 
@@ -90,7 +92,10 @@ LET candidates = (
       For vplankoppelingenwe still may call DOCUMENT(...) for each vplankoppeling (these are fewer)
 */
 FOR c IN candidates
-  LET vplankoppelingen = (
+  /* vplankoppelingen */
+  FOR vplan IN vplankoppelingen
+    FILTER vplan.asset_key == c._key
+/*  LET vplankoppelingen = (
     FOR vplan IN vplankoppelingen
       FILTER vplan.asset_key == c._key
       RETURN {
@@ -99,29 +104,64 @@ FOR c IN candidates
         inDienstDatum: vplan.inDienstDatum,
         uitDienstDatum: vplan.uitDienstDatum
       }
-  )
-  FILTER length(vplankoppelingen) > 0
+  )*/
+//  FILTER length(vplankoppelingen) > 0
 
-  // TO DO: voeg het aantal jaar toe dat het plan in dienst is. 
-  /*
-  provincie
-  gemeente
-  indienstdatum
-  uitdienstdatum
-  aantal_jaar_in_dienst
-  vplannummer
-  vplannummer_kort
-  commentaar? >> Mist in arangoDB ?
-  edeltadossiernummer >> Bestek
-  aannemernaam >> bestek
-  dataconflicten >> nazien hoe deze wordt gereconstrueerd.
-  */
+  /* adres */
+  LET adres_json = (c.loc && c.loc.Locatie_puntlocatie && c.loc.Locatie_puntlocatie.DtcPuntlocatie_adres) ? c.loc.Locatie_puntlocatie.DtcPuntlocatie_adres : null
+  LET provincie = adres_json && adres_json.DtcAdres_provincie ? adres_json.DtcAdres_provincie : null
+  LET gemeente = adres_json && adres_json.DtcAdres_gemeente ? adres_json.DtcAdres_gemeente : null
+  LET adres_parts = [
+    adres_json && adres_json.DtcAdres_straat ? adres_json.DtcAdres_straat : null,
+    adres_json && adres_json.DtcAdres_nummer ? adres_json.DtcAdres_nummer : null,
+    adres_json && adres_json.DtcAdres_bus ? adres_json.DtcAdres_bus : null,
+    adres_json && adres_json.DtcAdres_postcode ? adres_json.DtcAdres_postcode : null,
+    adres_json && adres_json.DtcAdres_gemeente ? adres_json.DtcAdres_gemeente : null
+  ]
+  LET adres_arr = (FOR p IN adres_parts FILTER p != null RETURN p)
+  LET adres = LENGTH(adres_arr) > 0 ? CONCAT_SEPARATOR(" ", adres_arr) : null
+
+  // bestekkoppelingen
+  LET asset_bestekken = (
+    FOR bk IN (c.bs && c.bs.Bestek_bestekkoppeling ? c.bs.Bestek_bestekkoppeling : [])
+        FILTER bk != null AND bk._to != null
+        LET b = DOCUMENT(bk._to)
+        FILTER b != null
+        RETURN {
+            dossiernummer: b.eDeltaDossiernummer ? b.eDeltaDossiernummer : null,
+            besteknummer: b.eDeltaBesteknummer ? b.eDeltaBesteknummer : null,
+            aannemer: b.aannemerNaam ? b.aannemerNaam : null,
+            van: bk.DtcBestekkoppeling_actiefVan ? bk.DtcBestekkoppeling_actiefVan : null,
+            tot: bk.DtcBestekkoppeling_actiefTot ? bk.DtcBestekkoppeling_actiefTot : null
+        }
+    )
+    LET meest_recent_bestek = asset_bestekken[0]
+
+  /* Calculate jarenInDiendst */
+  LET jarenInDienst =
+    IS_NULL(vplan.uitDienstDatum)
+      ? DATE_DIFF(left(vplan.inDienstDatum, 10), DATE_NOW(), "years")
+      : DATE_DIFF(left(vplan.inDienstDatum, 10), left(vplan.uitDienstDatum, 10), "years")
+  LET vplan_nummer_kort = vplan.vplan_nummer ? left(vplan.vplan_nummer, 7) : null
 
   RETURN {
     uuid: c._key,
     naam: c.naam,
     naampad: c.naampad,
     toestand: c.toestand,
-    vplankoppelingen: vplankoppelingen
+    vplan_uuid: vplan.vplan_uuid,
+    vplan_nummer: vplan.vplan_nummer,
+    vplan_nummer_kort: vplan_nummer_kort,
+    vplan_commentaar: null,
+    inDienstDatum: vplan.inDienstDatum,
+    uitDienstDatum: vplan.uitDienstDatum,
+    jarenInDienst: jarenInDienst,
+    provincie: provincie,
+    gemeente: gemeente,
+    adres: adres,
+    //alle_bestekken: asset_bestekken,
+    recent_bestek: meest_recent_bestek,
+    recent_bestek_dossiernummer: meest_recent_bestek.dossiernummer,
+    recent_bestek_aannemer: meest_recent_bestek.aannemer
   }
 """
