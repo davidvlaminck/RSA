6602989e56c76f401e9f63caa745b01cfb36b370
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-03-30 21:35:44 +0200

    cleanup of scripts

--- scripts/trace_fs_changes_inotify.py
+++
@@ -1,8 +0,0 @@
-# stub: trace_fs_changes_inotify removed
-# The user asked to undo the tracer scripts created earlier. This file was replaced with a harmless stub.
-import sys
-
-if __name__ == '__main__':
-    print('trace_fs_changes_inotify.py has been removed per user request. No action performed.')
-    sys.exit(0)
-
