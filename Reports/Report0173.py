3789070dd9bf0cdc87f2dcd91530ad7fc109acb5
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-06-12 11:19:18 +0200

    changed to AQL

--- Reports/Report0173.py
+++ Reports/Report0173.py
@@ -4,33 +4,39 @@ from lib.reports.BaseReport import BaseReport
 
 class Report0173(BaseReport):
     def init_report(self) -> None:
+        aql_query = """
+FOR a IN assets
+  FILTER a.AIMDBStatus_isActief == true
+  FILTER a.NaampadObject_naampad == null OR a.NaampadObject_naampad == ""
+  LET assettype = FIRST(
+    FOR at IN assettypes
+      FILTER at._key == a.assettype_key
+      FILTER REGEX_TEST(at.uri, '^https://lgc.data.wegenenverkeer.be')
+      LIMIT 1
+      RETURN at
+  )
+  FILTER assettype != null
+  SORT a.NaampadObject_naampad ASC, a.AIMNaamObject_naam ASC
+  RETURN {
+    uuid: a._key,
+    toestand: a.toestand,
+    actief: a.AIMDBStatus_isActief,
+    naam: a.AIMNaamObject_naam,
+    naampad: a.NaampadObject_naampad,
+    assettype_naam: assettype.label,
+    assettype_uri: assettype.uri
+  }
+"""
         self.report = DQReport(name='report0173',
                                title='Legacy assets ontbreken een naampad',
                                spreadsheet_id='1Lu1nSafM08GgwY1FHd4vnEN6qbS0Z4lI-H96gZzV8Z0',
-                               datasource='PostGIS',
+                               datasource='ArangoDB',
                                persistent_column='H',
                                link_type='eminfra',
                                excel_filename='[RSA] Legacy assets ontbreken een naampad.xlsx',
                                )
 
-        self.report.result_query = """
-        select
-            a."uuid"
-            , a.toestand
-            , a.actief 
-            , a.naam
-            , a.naampad
-            , at.naam as assettype_naam
-            , at.uri as assettype_uri
-        from assets a
-        left join assettypes at on a.assettype = at."uuid" 
-        where
-            a.actief is true 
-            and
-            a.naampad is null
-            and
-            at.uri ~ '^https://lgc.data.wegenenverkeer.be'
-        """
+        self.report.result_query = aql_query
 
     def run_report(self, sender) -> None:
         self.report.run_report(sender=sender)
