4c450be67aec9dadccb4fab04a2197c78867ea31
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-09-11 11:54:26 +0200

    changed to arango

--- Reports/Report0176.py
+++ Reports/Report0176.py
@@ -4,44 +4,37 @@ from lib.reports.BaseReport import BaseReport
 
 class Report0176(BaseReport):
     def init_report(self) -> None:
+        aql_query = """
+        FOR a1 IN assets
+          FILTER a1.assettype_key == "615356ae"
+          FILTER a1.AIMDBStatus_isActief == true
+          FILTER HAS(a1, "geometry")
+          FOR a2 IN assets
+            FILTER a2.assettype_key == "615356ae"
+            FILTER a2.AIMDBStatus_isActief == true
+            FILTER HAS(a2, "geometry")
+            FILTER a1._key != a2._key
+            LET dist = DISTANCE(a1.geometry.coordinates[1], a1.geometry.coordinates[0], a2.geometry.coordinates[1], a2.geometry.coordinates[0])
+            FILTER dist <= 5
+            RETURN {
+              g1_uuid: a1._key,
+              g1_toestand: SPLIT(a1.AIMToestand_toestand, '/')[-1],
+              g1_naam: a1.AIMNaamObject_naam,
+              g1_commentaar: a1.AIMObject_notitie,
+              g2_uuid: a2._key,
+              g2_toestand: SPLIT(a2.AIMToestand_toestand, '/')[-1],
+              g2_naam: a2.AIMNaamObject_naam,
+              g2_commentaar: a2.AIMObject_notitie,
+              afstand: FLOOR(dist * 100) / 100
+            }
+        """
+
         self.report = DQReport(name='report0176', title='Dubbele assets op kruispunt: Galgpaal, Seinbrug, Boogpaal, Rechte Steun',
-                               spreadsheet_id='1F9BXJrnr-hdoLnQ9L7UctGF16IyUQqlV7hRJzy1WlO0', datasource='PostGIS',
+                               spreadsheet_id='1F9BXJrnr-hdoLnQ9L7UctGF16IyUQqlV7hRJzy1WlO0', datasource='ArangoDB',
                                persistent_column='',
                                excel_filename='[RSA] Dubbele assets op kruispunt_ Galgpaal, Seinbrug, Boogpaal, Rechte Steun.xlsx',)
 
-        self.report.result_query = """
-        with cte_asset_galgpaal as (
-            select
-                a.uuid
-                , a.toestand
-                , a.naam
-                , a.commentaar
-                , g.geometry
-            from assets a
-            left join geometrie g on a.uuid = g.assetuuid
-            where
-                a.actief is true 
-                and
-                a.assettype = '615356ae-64eb-4a7d-8f40-6e496ec5b8d7'  -- Galgpaal
-        )
-        select	
-            a1.uuid as g1_uuid
-            , a1.toestand as g1_toestand
-            , a1.naam as g1_naam
-            , a1.commentaar as g1_commentaar
-            , a2.uuid as g2_uuid
-            , a2.toestand as g2_toestand
-            , a2.naam as g2_naam
-            , a2.commentaar as g2_commentaar
-            , round(ST_Distance(a1.geometry, a2.geometry)::numeric, 2) as afstand
-        from cte_asset_galgpaal a1
-        --Join 2 Galgpalen binnen een afstand van 5 meter.
-        --Dit is niet per se foutief, maar zo missen we ook de kleinste afstanden niet.
-        left join cte_asset_galgpaal a2 on st_dwithin(a1.geometry, a2.geometry, 5)
-        where
-            a1.uuid <> a2.uuid  -- geen join van een Galgpaal met zichzelf
-        order by afstand desc
-	    """
+        self.report.result_query = aql_query
 
     def run_report(self, sender) -> None:
         self.report.run_report(sender=sender)
