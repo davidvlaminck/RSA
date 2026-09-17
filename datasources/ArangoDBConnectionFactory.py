3d8d282502238a5e2a0b0a316c67ed8cf856865e
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2026-09-17 20:05:18 +0200

    more time

--- datasources/ArangoDBConnectionFactory.py
+++ datasources/ArangoDBConnectionFactory.py
@@ -3,7 +3,7 @@ from arango import ArangoClient
 
 class ArangoDBConnectionFactory:
     def __init__(self, db_name, username, password):
-        self.client = ArangoClient(request_timeout=180)
+        self.client = ArangoClient(request_timeout=360)
         self.db_name = db_name
         self.username = username
         self.password = password
